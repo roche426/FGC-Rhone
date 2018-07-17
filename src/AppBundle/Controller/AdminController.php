cae37a16016d7af4180c0a189d359a19f3a93c41
Aurélien Roche <roche426@gmail.com>
2018-07-17 11:36:51 +0200

    visualiser users + articles publiés

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Blog;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,9 +17,34 @@ class AdminController extends Controller
     /**
      * @Route("/", name="admin_home")
      */
-    public function ConnectedAction()
+    public function connectedAction()
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="admin_users")
+     */
+    public function showUsersAction()
+    {
+        $em = $this->getDoctrine()->getRepository(User::class);
+        $users = $em->findAll();
+
+        return $this->render('admin/users.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/users/{id}", name="admin_see_user")
+     */
+    public function showOneUserAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository(User::class)->find($id);
+        $articles = $em->getRepository(Blog::class)->findAll($id);
+
+        return $this->render('admin/showUser.html.twig', [
+            'user' => $user,
+            'articles' => $articles]);
     }
 
 
