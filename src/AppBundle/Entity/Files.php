1c1ca4face566559f63c32a9cf75896ed6254350
Aurélien Roche <roche426@gmail.com>
2018-08-25 14:50:06 +0200

    ajout autres documents en BDD

--- src/AppBundle/Entity/Files.php
+++ src/AppBundle/Entity/Files.php
@@ -31,6 +31,38 @@ class Files
      */
     private $idCard;
 
+    /**
+     * @var string
+     * @ORM\Column(name="files2", type="string",nullable=true)
+     */
+    private $files2;
+
+    /**
+     * @var string
+     * @ORM\Column(name="files3", type="string",nullable=true)
+     */
+    private $files3;
+
+    /**
+     * @var string
+     * @ORM\Column(name="files4", type="string",nullable=true)
+     */
+    private $files4;
+
+    /**
+     * @var string
+     * @ORM\Column(name="files5", type="string",nullable=true)
+     */
+    private $files5;
+
+    /**
+     * @var string
+     * @ORM\Column(name="files6", type="string",nullable=true)
+     */
+    private $files6;
+
+
+
     public function __construct()
     {
         $this->user = new ArrayCollection();
@@ -67,6 +99,87 @@ class Files
     {
         $this->idCard = $idCard;
     }
+
+    /**
+     * @return string
+     */
+    public function getFiles2()
+    {
+        return $this->files2;
+    }
+
+    /**
+     * @param string $files2
+     */
+    public function setFiles2($files2)
+    {
+        $this->files2 = $files2;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFiles3()
+    {
+        return $this->files3;
+    }
+
+    /**
+     * @param string $files3
+     */
+    public function setFiles3($files3)
+    {
+        $this->files3 = $files3;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFiles4()
+    {
+        return $this->files4;
+    }
+
+    /**
+     * @param string $files4
+     */
+    public function setFiles4($files4)
+    {
+        $this->files4 = $files4;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFiles5()
+    {
+        return $this->files5;
+    }
+
+    /**
+     * @param string $files5
+     */
+    public function setFiles5($files5)
+    {
+        $this->files5 = $files5;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFiles6()
+    {
+        return $this->files6;
+    }
+
+    /**
+     * @param string $files6
+     */
+    public function setFiles6($files6)
+    {
+        $this->files6 = $files6;
+    }
+
 
     /**
      * @return mixed
